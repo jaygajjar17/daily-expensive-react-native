63cfde70d96afc3d858f3f8549cbb3ed6cfcbce8
Jay Gajjar <48956611+jaygajjar17@users.noreply.github.com>
2022-12-19 17:52:52 +0530

    Update firebase.js

--- src/service/firebase.js
+++ src/service/firebase.js
@@ -2,12 +2,12 @@ import * as firebase from "firebase";
 import firestore from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAKnJmHgohOKCQudNt4tWNhsioiz3UW8IM",
-  authDomain: "YOUR-PROJECT-NAME.firebaseapp.com",
-  databaseURL: "https://daily-expense-5cc43-default-rtdb.firebaseio.com",
-  projectId: "daily-expense-5cc43",
-  storageBucket: "daily-expense-5cc43.appspot.com",
-  appId: "1:733353459239:ios:9bc90d24d2f043f00dd802",
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  appId: "",
 };
 firebase.initializeApp(firebaseConfig);
 firebase.firestore();
